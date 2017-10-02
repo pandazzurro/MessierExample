2b679ac837911121f323ade31a6a2d91c04c7ba5
Andrea Tosato <atosato@alturas.it>
2017-10-02 10:31:57 +0200

    Example project

---
+++ MessierSample/MessierSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessierSample.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private decimal _distanceProgress = 0;
+        public decimal DistanceProgress
+        {
+            get { return _distanceProgress; }
+            set { SetProperty(ref _distanceProgress, value); }
+        }
+
+        public MainPageViewModel()
+        {
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+                {
+                    DistanceProgress = (int)DateTime.Now.Second / 60;
+                    return true;
+                });
+            });
+        }
+    }
+}
---
+++ MessierSample/MessierSample/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MessierSample.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
